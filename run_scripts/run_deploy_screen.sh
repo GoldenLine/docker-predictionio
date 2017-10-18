4bcb9a20b8bd7174e638080a6e0ed752f53a527f
Michał Szyszewski <Szyszka@MacBook-Pro-Micha.local>
2017-10-18 10:09:46 +0200

    Skrypty uruchamiajace screeny

---
+++ run_scripts/run_deploy_screen.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+docker exec pio_v0.10_project_candidates  /bin/sh -c "/universal-recommender/cron/screen_deploy.sh"
---
+++ run_scripts/run_remove_deploy_screen.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+docker exec pio_v0.10_project_candidates  /bin/sh -c "/universal-recommender/cron/remove_old_screen.sh"
